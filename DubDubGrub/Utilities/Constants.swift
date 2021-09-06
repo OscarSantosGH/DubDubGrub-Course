6a583bd540a046659bb102b0618aaa55e749af9f
Oscar Santos <osantos@tql.com>
2021-09-06 16:35:44 -0400

    add: Constants file

---
+++ DubDubGrub/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  DubDubGrub
+//
+//  Created by Oscar Santos on 9/6/21.
+//
+
+import Foundation
+
+enum RecordType {
+    static let location = "DDGLocation"
+    static let profile = "DDGProfile"
+}
