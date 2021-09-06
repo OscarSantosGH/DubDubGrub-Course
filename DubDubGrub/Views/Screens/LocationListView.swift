1f9460696b58d6ea11150f52e9a4ce367fd7f698
Oscar Santos <osantos@tql.com>
2021-09-06 15:42:24 -0400

    mod: LocationListView initialization with a DDGLocation array binding var

--- DubDubGrub/Views/Screens/LocationListView.swift
+++ DubDubGrub/Views/Screens/LocationListView.swift
@@ -8,11 +8,14 @@
 import SwiftUI
 
 struct LocationListView: View {
+    
+    @State private var locations: [DDGLocation] = [DDGLocation(record: MockData.location)]
+    
     var body: some View {
         NavigationView {
-            List(0..<11) { _ in
-                NavigationLink(destination: LocationDetailView()) {
-                    LocationCell()
+            List(locations, id: \.ckRecordID) { location in
+                NavigationLink(destination: LocationDetailView(location: location)) {
+                    LocationCell(location: location)
                 }
             }
             .navigationTitle("Grub Spots")
