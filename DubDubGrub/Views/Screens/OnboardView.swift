e07942d0d972251abb4df497f6944ec35e30d9c3
Oscar Santos <osantos@tql.com>
2022-10-08 13:05:01 -0700

    added: OnboardView

---
+++ DubDubGrub/Views/Screens/OnboardView.swift
@@ -0,0 +1,75 @@
+//
+//  OnboardView.swift
+//  DubDubGrub
+//
+//  Created by Oscar Santos on 10/8/22.
+//
+
+import SwiftUI
+
+struct OnboardView: View {
+    var body: some View {
+        VStack{
+            HStack{
+                Spacer()
+                Button{
+                    
+                } label: {
+                    ZStack{
+                        Circle()
+                            .frame(width: 30, height: 30)
+                            .foregroundColor(.brandPrimary)
+                        Image(systemName: "xmark")
+                            .foregroundColor(.white)
+                            .imageScale(.small)
+                            .frame(width: 44, height: 44)
+                    }
+                }
+            }
+            LogoView(frameWidth: 250)
+                .padding(.bottom)
+            
+            VStack(alignment: .leading, spacing: 32){
+                OnboardingInfoView(imageName: "building.2.crop.circle",
+                                   title: "Restaurant Locations",
+                                   description: "Find places ti dine around the convention center")
+                OnboardingInfoView(imageName: "checkmark.circle",
+                                   title: "Check In",
+                                   description: "Let other iOS Devs know where you are")
+                OnboardingInfoView(imageName: "building.2.crop.circle",
+                                   title: "Find Friends",
+                                   description: "See where other iOS Devs are and join the party")
+            }
+        }
+        .padding(.horizontal, 40)
+    }
+}
+
+struct OnboardView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardView()
+    }
+}
+
+struct OnboardingInfoView: View {
+    
+    var imageName: String
+    var title: String
+    var description: String
+    
+    var body: some View {
+        HStack(spacing: 26){
+            Image(systemName: imageName)
+                .resizable()
+                .frame(width: 50, height: 50)
+                .foregroundColor(.brandPrimary)
+            VStack(alignment: .leading, spacing: 4){
+                Text(title).bold()
+                Text(description)
+                    .foregroundColor(.secondary)
+                    .lineLimit(2)
+                    .minimumScaleFactor(0.75)
+            }
+        }
+    }
+}
